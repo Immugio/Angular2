d3f0af0158f5f8e9ddd25d2f76c61a4086878ff8
Immugio <janmikeska@googlemail.com>
2016-01-22 16:02:33 +0100

    https://angular.io/docs/ts/latest/tutorial/toh-pt1.html

---
+++ src/AspNet5Angular2Demo/scripts/IHero.ts
@@ -0,0 +1,4 @@
+﻿interface IHero {
+    id: number;
+    name: string;
+}
--- src/AspNet5Angular2Demo/scripts/app.ts
+++ src/AspNet5Angular2Demo/scripts/app.ts
@@ -2,8 +2,21 @@
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+        </div>
+        `
 })
 export class AppComponent {
 
+    public title = "Tour of Heroes";
+    public hero: IHero = {
+        id: 1,
+        name: "Windstorm"
+    };
 }
