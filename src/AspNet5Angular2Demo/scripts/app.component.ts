89bf0d8942efbafc24c909595020fe77c1f7087b
Immugio <janmikeska@googlemail.com>
2016-02-01 11:24:01 +0100

    Hero functionality

---
+++ src/AspNet5Angular2Demo/scripts/app.component.ts
@@ -0,0 +1,29 @@
+﻿import {Component} from "angular2/core";
+import {OnInit} from "angular2/core";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
+
+@Component({
+    selector: "my-app",
+    templateUrl: "my-app.html",
+    styleUrls: ["my-app-styles.css"],
+    directives: [HeroDetailComponent],
+    providers : [HeroService]
+})
+export class AppComponent implements OnInit {
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    public heroes: IHero[];
+    public title = "Tour of Heroes";
+    public selectedHero: IHero;
+    public onSelect(hero: IHero) { this.selectedHero = hero; }
+
+    getHeroes() {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+}
---
+++ src/AspNet5Angular2Demo/scripts/hero-detail.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from "angular2/core";
+
+@Component({
+    selector: "my-hero-detail",
+    templateUrl: "hero-detail.html",
+    inputs: ["hero"]
+})
+export class HeroDetailComponent {
+    public hero: IHero;
+}
